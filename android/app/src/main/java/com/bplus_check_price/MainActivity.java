d2bd2bd162ef3c11a3e003eaf632604a7bf6fbfc
blacksakura013 <s.cainnawat@gmail.com>
2021-12-20 08:51:37 +0700

    start

---
+++ android/app/src/main/java/com/bplus_check_price/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bplus_check_price;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Bplus_Check_Price";
+  }
+}
